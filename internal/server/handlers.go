66fee41cc5f5c22c931b929f8682f483d4d53f1b
ante-neh <amareanteneh12@gmail.com>
2024-08-11 17:02:48 +0300

    feat(web):add get user handler

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -1,7 +1,25 @@
 package server
 
-import "net/http"
+import (
+	"net/http"
+	"github.com/ante-neh/memory-chache/util"
+)
 
 func (s *Server) handleGetUser(w http.ResponseWriter, r *http.Request){
+	id := r.URL.Query().Get("id") 
 	
+	user, ok := s.Chache.GetUserById(id) 
+
+	if !ok {
+		util.ResponseWithJson(w, 200, user)
+	}
+
+	user, ok = s.Db.GetUserById(id)
+
+	if ok{
+		util.ResponseWithError(w, 404, "user not found")
+	}
+
+	s.Chache.AddUser(id, *user)
+	util.ResponseWithJson(w, 200, user)
 }
