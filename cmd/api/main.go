bf7f375c6b5b95df60afbe6143079a594da884a0
ante-neh <amareanteneh12@gmail.com>
2024-08-11 14:02:20 +0300

    feat(backend):set up the server

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"log"
+	"os"
+	"github.com/ante-neh/memory-chache/internal/server"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello there this is brong english")
+	//Load environmental varaibles 
+	err := godotenv.Load()
+	if err != nil{
+		log.Fatal("Unable to load environmental varaibles")
+	}
+
+    //access port address and set it a default value
+	port := os.Getenv("PORT")
+	address := flag.String("address", port, "server address")
+	flag.Parse()
+
+	//create info and error loggers to handle errors gracefully
+	infoLog := log.New(os.Stdout, "INFO: ", log.Ltime | log.Ldate) 
+	errorLog := log.New(os.Stdout, "ERROR: ", log.Ltime | log.Ldate | log.Lshortfile) 
+
+	//create a server type 
+	app := server.NewServer(infoLog, errorLog, *address)
+
+	//start the server
+	server := app.Start()
+
+	app.InfoLogger.Println("Server is running on port: ", *address)
+	err = server.ListenAndServe()
+	
+	if err != nil{
+		app.ErrorLogger.Fatal("Unable to start a server")
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/ante-neh/memory-chache
 
 go 1.22.6
+
+require github.com/joho/godotenv v1.5.1
